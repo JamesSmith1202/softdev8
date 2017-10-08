1a6ed218ef8ef8a3dcb5a1b8eacfc2932fa502b1
JamesSmith1202 <jsmith@stuy.edu>
2017-10-08 00:07:55 -0400

    added url_for to redirect and cleaned up logic for secure login

--- work08/work08.py
+++ work08/work08.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 
 USERNAME = "yes"
 PASSWORD = "no"
@@ -9,41 +9,47 @@ app.secret_key = 'super secret key'
 @app.route("/")   
 def root():
     if 'username' in session:
-        return redirect('welcome')
+        return redirect(url_for('welcome'))
     else:
-        return redirect('login')
+        return redirect(url_for('login'))
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    response = None
-    errormessage = ""
-    if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
-        if(username == USERNAME):
-            if(password == PASSWORD):
-                session['username'] = username
-                return redirect('welcome')
+    if not 'username' in session:
+        response = None
+        errormessage = ""
+        if request.method == "POST":
+            username = request.form['username']
+            password = request.form['password']
+            if(username == USERNAME):
+                if(password == PASSWORD):
+                    session['username'] = username
+                    return redirect(url_for('welcome'))
+                else:
+                    errormessage = "Incorrect password"
             else:
-                errormessage = "Incorrect password"
-        else:
-            errormessage = "Incorrect username and maybe password"
-    return render_template('login.html', errormessage = errormessage)
-
+                errormessage = "Incorrect username and maybe password"
+        return render_template('login.html', errormessage = errormessage)
+    else:
+        return redirect(url_for('welcome'))
+    
 @app.route("/welcome", methods=['GET', 'POST'])
 def welcome():
     if request.method == "POST":
-        return redirect('logout')
+        return redirect(url_for('logout'))
     else:
         if not ('username' in session):
-            return redirect('login')
+            return redirect(url_for('login'))
         else:
             return render_template('welcome.html', username = USERNAME)
     
 @app.route("/logout")
 def logout():
-    session.pop('username')
-    return render_template("logout.html", username = USERNAME)
+    if 'username' in session:
+        session.pop('username')
+        return render_template("logout.html", username = USERNAME)
+    else:
+        return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.debug = True
